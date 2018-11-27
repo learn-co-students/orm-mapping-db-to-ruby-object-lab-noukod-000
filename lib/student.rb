9c720b157d840f6e5506d31e19157ae74934f4f8
Merline Belizaire <merline@noukod.com>
2018-11-27 15:49:28 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -23,7 +23,8 @@ class Student
 
   def self.find_by_name(name)
     # find the student in the database given a name
-    # return a new instance of the Student class    sql = <<-SQL
+    # return a new instance of the Student class
+    sql = <<-SQL
       SELECT *
       FROM songs
       WHERE name = ?
